61c926297821bc8e635e07ed995b597ff36df503
Bradley_Norman <Bradley.Norman91@gmail.com>
2015-11-24 07:40:26 -0500

    Changes

---
+++ FurnitureRentalStore/DAL/Repository/RentalTransactionRepository.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using FurnitureRentalStore.DAL.Interfaces;
+using FurnitureRentalStore.Model;
+using MySql.Data.MySqlClient;
+
+namespace FurnitureRentalStore.DAL.Repository
+{
+    class RentalTransactionRepository : IRepository<RentalTransaction>
+    {
+        private readonly string connectionString;
+
+        private int returnTransactionId;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RentalRepository"/> class.
+        /// </summary>
+        public RentalTransactionRepository()
+        {
+            this.connectionString = ConfigurationManager.ConnectionStrings["MySqlDbConnection"].ToString();
+        }
+
+        /// <summary>
+        /// Gets the transaction identifier.
+        /// </summary>
+        /// <returns></returns>
+        public int GetTransactionId()
+        {
+            return this.returnTransactionId;
+        }
+
+        /// <summary>
+        /// Adds the specified entity.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public void Add(RentalTransaction entity)
+        {
+            const string query = "INSERT INTO RENTAL_TRANSACTION(employeeID, memberID, date, totalPrice) VALUES(@employeeID, @memberID, @date, @totalPrice);select last_insert_id();";
+
+            try
+            {
+                using (var conn = new MySqlConnection(this.connectionString))
+                {
+                    conn.Open();
+
+                    using (var cmd = new MySqlCommand(query, conn))
+                    {
+                        ParameterizeQuery(entity, cmd);
+                        this.returnTransactionId = Convert.ToInt32(cmd.ExecuteScalar());
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Parameterizes the query.
+        /// </summary>
+        /// <param name="aTransaction">a member.</param>
+        /// <param name="cmd">The command.</param>
+        private static void ParameterizeQuery(RentalTransaction aTransaction, MySqlCommand cmd)
+        {
+            cmd.Parameters.Add("@employeeID", MySqlDbType.Int32);
+            cmd.Parameters.Add("@memberID", MySqlDbType.Int32);
+            cmd.Parameters.Add("@date", MySqlDbType.DateTime);
+            cmd.Parameters.Add("@totalPrice", MySqlDbType.Double);
+
+            cmd.Parameters["@employeeID"].Value = aTransaction.EmployeeId;
+            cmd.Parameters["@memberID"].Value = aTransaction.MemberId;
+            cmd.Parameters["@date"].Value = aTransaction.Date;
+            cmd.Parameters["@totalPrice"].Value = aTransaction.TotalPrice;
+
+            cmd.ExecuteNonQuery();
+        }
+
+        /// <summary>
+        /// Gets the by identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public RentalTransaction GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets all.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public List<RentalTransaction> GetAll()
+        {
+            var transactions = new List<RentalTransaction>();
+            const string query = "select rentalTransactionID, employeeID, memberID, date, totalPrice from Rental_Transaction";
+
+            try
+            {
+                using (var conn = new MySqlConnection(this.connectionString))
+                {
+                    conn.Open();
+
+                    using (var cmd = new MySqlCommand(query, conn))
+                    {
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            PopulateTransactions(reader, transactions);
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return transactions;
+        }
+
+        /// <summary>
+        /// Populates the members.
+        /// </summary>
+        /// <param name="reader">The reader.</param>
+        /// <param name="transactions">The transactions.</param>
+        private static void PopulateTransactions(MySqlDataReader reader, List<RentalTransaction> transactions)
+        {
+            while (reader.Read())
+            {
+                var entity = new RentalTransaction
+                {
+                    RentalTransactionId = Convert.ToInt32(reader["rentalTransactionID"].ToString()),
+                    EmployeeId = Convert.ToInt32(reader["employeeID"]),
+                    MemberId = Convert.ToInt32(reader["memberID"]),
+                    Date = Convert.ToDateTime(reader["date"]),
+                    TotalPrice = Convert.ToInt32(reader["totalPrice"]),
+                };
+
+
+                transactions.Add(entity);
+            }
+        }
+    }
+}
